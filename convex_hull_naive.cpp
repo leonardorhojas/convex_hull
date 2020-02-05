e92570110e6347b37c110f50a61d4a82dadbead5
leonardorhojas <leonardorhojas@gmail.com>
2020-02-04 21:57:44 -0500

    c7

--- convex_hull_naive.cpp
+++ convex_hull_naive.cpp
@@ -81,9 +81,9 @@ int main(int argc, char* argv[])
  for(int i = 0; i < resulting_segments.size(); i++){
     std::cout << resulting_segments[i] << std::endl;
     if(points[1]<points[3]){
-     std::cout << "Resulti1 < result2"<< std::endl; 
+     std::cout << "Resulti1 < result2" + points[1] +", "+ points 3<< std::endl; 
    } else{
-      std::cout << "Resulti1 > result2"<< std::endl; 
+      std::cout << "Resulti1 > result2" + points[1] +", "+ points 3 << std::endl; 
 
    }
 
