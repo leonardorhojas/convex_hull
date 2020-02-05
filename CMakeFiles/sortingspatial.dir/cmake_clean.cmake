902297d3098307462c91ebe019d58533c71cae43
root <root@ip-172-31-84-133.ec2.internal>
2020-02-05 12:55:06 +0000

    e1

---
+++ CMakeFiles/sortingspatial.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sortingspatial.dir/sortingspatial.cpp.o"
+  "sortingspatial.pdb"
+  "sortingspatial"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sortingspatial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
