cd3624fa2b43553891a81db50b13fa6e6de79910
leonardorhojas <leonardorhojas@gmail.com>
2020-02-13 02:36:16 -0500

    area3

--- convex_hull_naive.cpp
+++ convex_hull_naive.cpp
@@ -185,7 +185,7 @@ results.assign(result_set.begin(),result_set.end());
 
 Print_Vector(results);
 area1=GreenArea(results);
-std::cout<<area1<<std::endl;
+std::cout<<"GREEN Area Naive algorithm:"<<area1<<std::endl;
 //sort(results.begin(),results.end());
 std::cout<<"******"<<std::endl;
  
@@ -198,7 +198,7 @@ CGAL::convex_hull_2( points.begin(), points.end(), std::back_inserter(results3) 
 //for(int i=0; i<results.size(); i++)std::cout<<results[i]<<  ", "<< results2[i]<<std::endl;//output
 Print_Vector(results3);
 area2=GreenArea(results);
-std::cout<<area2<<std::endl;
+std::cout<<"GREEN Area Yarvis algorithm:"<<area2<<std::endl;
 
 return 0;
 }
