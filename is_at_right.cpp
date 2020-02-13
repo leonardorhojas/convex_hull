03ad02d9bea86a1f4bee640f0437e4654d7e6b0a
leonardorhojas <leonardorhojas@gmail.com>
2020-02-13 00:21:47 -0500

    incremental_16

--- is_at_right.cpp
+++ is_at_right.cpp
@@ -40,14 +40,20 @@ bool IsPointAtRight( const _TPoint& p, const _TPoint& q, const _TPoint& r )
 {
 	Point_2 p,q,r;
 	 bool r_right_of_pq;
+ 	 bool r_right_of_pq_2;
+
 		p=( Point_2( -7.92352 ,-3.49278) );
 		q=( Point_2( -1.7415, 4.23544 ) );
 		r=( Point_2( -0.0153198, -8.51879 ) );
 	r_right_of_pq=CGAL::right_turn (p, q, r);
-// r_right_of_pq=IsPointAtRight( p, q, r );
+    r_right_of_pq_2=IsPointAtRight( p, q, r );
 	if (r_right_of_pq==true){
 		  std::cout<<"r is at right of pq"<<std::endl;
 	}else{  std::cout<<"r is NOT at right of pq"<<std::endl;}
+	std::cout<<"================="<<std::endl;
+if (r_right_of_pq_2==true){
+		  std::cout<<"r_is_right_of_pq"<<std::endl;
+	}else{  std::cout<<"r_is NOT at_right_of_pq"<<std::endl;}
 
 	return 0;
 
